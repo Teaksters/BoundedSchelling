f52dd90ddabe909e6b871b75f9f5e42ffa216e1e
Teaksters <arneb.meijs@gmail.com>
2019-01-10 18:44:29 +0100

    Just need utility function

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 def main():
-    a = schelling.Schelling([50, 100, 80], [[0.5, 0.2], [0.1, 0.2], [0.5, 0.3]])
-    a.run(500)
+    a = schelling.Schelling([50, 100, 80, 305], [[0.5, 0.2], [0.1, 0.2], [0.5, 0.3], [0.5, 0.3]])
+    a.run(10000)
     print(np.array([[agent.type for agent in n] for n in a.model.neighborhoods]))
+    print(a.model.density)
 
 if __name__ == '__main__':
     main()
